1344e984626e5c86481940ec0632512df5803e42
iamwrm <iamwrm@gmail.com>
2022-10-16 00:50:34 +0800

    fixing c warning

--- cxx/src/c_main.c
+++ cxx/src/c_main.c
@@ -4,10 +4,11 @@
 
 int on_data_in_c(int data){
     printf("ondata from c: %d\n",data);
+    return 0;
 }
 
 int main(){
     //create_signal_generator();
     //create_ondata();
-    register_callback(on_data_in_c);
+    register_callback((rust_callback)on_data_in_c);
 }
